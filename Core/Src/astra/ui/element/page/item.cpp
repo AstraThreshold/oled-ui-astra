51bf465d92ff12ab5e17b96d2b6bc5de6a45411b
forpaindream <fir@live.it>
2024-02-26 21:35:23 +0800

    if i pass the exam...?

--- Core/Src/astra/ui/element/page/item.cpp
+++ Core/Src/astra/ui/element/page/item.cpp
@@ -62,24 +62,6 @@ void Camera::goDirect(uint8_t _x, uint8_t _y) {
   y = _y;
 }
 
-Selector::Selector() {
-  this->x = 0;
-  this->xTrg = 0;
-  this->y = 0;
-  this->yTrg = 0;
-  this->w = 0;
-  this->wTrg = 0;
-}
-
-void Selector::render() {
-
-}
-
-void Selector::go(uint8_t _x, uint8_t _y) {
-  animation(&x, _x, astraConfig.selectorAnimationSpeed);
-  animation(&y, _y, astraConfig.selectorAnimationSpeed);
-}
-
 Menu::Menu(std::string _title) {
   this->title = std::move(_title);
   this->selfType = LIST;
@@ -108,7 +90,7 @@ Menu::Menu(std::string _title, std::vector<std::vector<uint8_t>> _pic) {
   this->child.clear();
 }
 
-void Menu::render(Camera* _camera, Selector* _selector) {
+void Menu::render(Camera* _camera) {
   if (selfType == TILE) {
     //一些前景的坐标
     static float yArrow, yArrowTrg;
@@ -180,7 +162,6 @@ void Menu::render(Camera* _camera, Selector* _selector) {
         }
       }
 
-      //todo 所有都没加入选择器
       HAL::drawChinese(_iter->position.x + _camera->x, _iter->position.y + _camera->y, _iter->title);
       animation(&_iter->position.y, _iter->position.yTrg, astraConfig.menuAnimationSpeed);
     }
@@ -239,4 +220,45 @@ bool Menu::addItem(Menu *_page) {
     } else return false;
   }
 }
+
+Selector::Selector() {
+  this->x = 0;
+  this->xTrg = 0;
+  this->y = 0;
+  this->yTrg = 0;
+  this->w = 0;
+  this->wTrg = 0;
+}
+
+bool Selector::inject(Menu *_menu) {
+  if (_menu == nullptr) return false;
+  if (this->menu != nullptr) return false;
+}
+
+bool Selector::destroy() {
+  if (this->menu == nullptr) return false;
+
+  delete this->menu;
+  this->menu = nullptr;
+}
+
+void Selector::render(Camera* _camera) {
+  ////todo 还没加入摄像机
+  if (menu->selfType == Menu::TILE) {
+
+  } else if (menu->selfType == Menu::LIST) {
+    HAL::setDrawType(2);
+    HAL::drawRBox(x, y, w, astraConfig.listLineHeight, astraConfig.selectorRadius);
+    HAL::setDrawType(1);
+  }
+}
+
+void Selector::go(uint8_t _x, uint8_t _y) {
+  animation(&x, _x, astraConfig.selectorAnimationSpeed);
+  animation(&y, _y, astraConfig.selectorAnimationSpeed);
+}
+
+
+
+
 }
--- Core/Src/astra/ui/element/page/item.h
+++ Core/Src/astra/ui/element/page/item.h
@@ -50,25 +50,8 @@ public:
   void reset();
 };
 
-class Selector : public Item, public Animation {
-public:
-  float x, xTrg;
-  float y, yTrg;
-  float w, wTrg;
-
-  Selector();
-
-  inline void render();
-
-  //在启动器中新建selector和camera 然后注入menu render
-  //在启动器中执行下述方法即可实现选择
-  //todo 当前选择的指针由启动器更改 然后get到对应的坐标给到selector
-  void go(uint8_t _x, uint8_t _y);
-
-};
-
 class Menu : public Item, public Animation {
-protected:
+public:
   //存储其在父页面中的位置
   //list中就是每一项对应的坐标 tile中就是每一个图片的坐标
   typedef struct Position {
@@ -98,13 +81,47 @@ public:
   explicit Menu(std::string _title);
   Menu(std::string _title, std::vector<std::vector<uint8_t>> _pic);
 
-  inline void render(Camera* _camera, Selector* _selector);  //render all child item.
+  inline void render(Camera* _camera);  //render all child item.
   [[nodiscard]] uint8_t getItemNum() const;
   [[nodiscard]] Position getItemPosition(uint8_t _index) const;
   [[nodiscard]] Menu* getNext() const;  //启动器调用该方法来获取下一个页面
   [[nodiscard]] Menu* getPreview() const;
 
+  //selector是启动器中修改的
+
   bool addItem(Menu* _page);
+};
+
+class Selector : public Item, public Animation {
+private:
+  Menu* menu;
+
+public:
+  float x, xTrg;
+  float y, yTrg;
+  float w, wTrg;
+
+  Selector();
+  ////todo!!!!!!! 最牛逼的来了 在磁贴中 文字和大框就是selector
+  ////todo 这样就可以弄磁贴的文字出现动画了
+  ////todo 在磁贴中 选择的时候 摄像机和selector都要移动 磁贴的selector是一个空心方框 + 底部的字体
+
+  bool inject(Menu* _menu); //inject menu instance to prepare for render.
+  bool destroy(); //destroy menu instance.
+
+  inline void render(Camera* _camera);
+
+  //在启动器中新建selector和camera 然后注入menu render
+  //在启动器中执行下述方法即可实现选择
+  //todo 当前选择的指针由启动器更改 然后get到对应的坐标给到selector
+
+  ////todo 但是不知道磁贴页的selector是怎么显示的
+  ////todo 想想怎么以更优雅的方式获取到磁贴页的标题
+  ////todo 想想怎么以更优雅的方式获取到各种坐标 看看能不能传个结构体进去
+
+  ////todo 考虑在Menu后面声明这一坨 然后在启动器里单独渲染selector
+  ////todo 而且selector的坐标也是相对于摄像机的坐标的
+  void go(uint8_t _x, uint8_t _y);
 };
 }
 #endif //ASTRA_ASTRA__H
