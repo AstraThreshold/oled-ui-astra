40b27fdd6213499846cf48ddc1c9f78f51c972b9
forpaindream <fir@live.it>
2024-04-26 21:31:22 +0800

    streamlined animation logic for efficiency.

--- Core/Src/astra/ui/item/item.h
+++ Core/Src/astra/ui/item/item.h
@@ -88,9 +88,8 @@ inline void Animation::blur() {
 }
 
 inline void Animation::move(float *_pos, float _posTrg, float _speed) {
-  if (std::fabs(_posTrg - *_pos) <= 1.0f) *_pos = _posTrg;
   if (*_pos != _posTrg) {
-    if (std::fabs(*_pos - _posTrg) < 0.15f) *_pos = _posTrg;
+    if (std::fabs(*_pos - _posTrg) <= 1.0f) *_pos = _posTrg;
     else *_pos += (_posTrg - *_pos) / ((100 - _speed) / 1.0f);
   }
 }
